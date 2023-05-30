14e2a63108d6f7374cdece65a4d7c481b3ab5bf2
PedroAlmeida18 <pedrohas35@gmail.com>
2023-05-30 15:14:18 -0300

    PROJETO01 - SEGUNDO TÓPICO DO CURSO; ATRIBUINDO VALORES AO FILME

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,12 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("aLÔ MUNDO !");
+        System.out.printf("Esse é Watch Has!");
+        System.out.println(" Filme :Top Gun : Maverick");
+        int anoDeLancamento = 2022;
+        System.out.println("Ano de Lançamento :" + anoDeLancamento);
+        boolean incluidoNoPlano= true;
+        double notaDoFilme = 8.1;
 
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
 
