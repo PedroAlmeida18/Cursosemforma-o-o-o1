178c323045bec6f57741eec1d0a5c4e8e411e088
PedroAlmeida18 <pedrohas35@gmail.com>
2023-06-01 09:08:24 -0300

    PROJETO01 - SEGUNDO TÓPICO, AULA SOBRE STRING, CONVERSÕES E COMENTÁRIOS

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,26 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Esse é Watch Has!");
+        /*System.out.printf("Esse é Watch Has!");
         System.out.println(" Filme :Top Gun : Maverick");
         int anoDeLancamento = 2022;
         System.out.println("Ano de Lançamento :" + anoDeLancamento);
         boolean incluidoNoPlano= true;
         double notaDoFilme = 8.1;
-
+        double media = (8.1+4.7+7.0)/3;
+        System.out.println(media);
+        // Cometário : Para fechar um bloco é interresante usar /* para abrir e fechar em /*
+        String sinpse ;
+       sinpse = """ 
+        Filme de aventura
+        o filme top gun é um filme legal  
+        bem feito e que fez milhões 
+        ano de lançamento em 
+        """ + anoDeLancamento;
+        System.out.println(sinpse);
+        int classificacao ;
+        classificacao = (int) (media/2);
+        System.out.println(classificacao);*/
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
 
     }
