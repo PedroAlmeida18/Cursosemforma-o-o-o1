be8164a183b43dbd05778a167342d9d9f1148ec2
PedroAlmeida18 <pedrohas35@gmail.com>
2023-06-01 09:27:08 -0300

    PROJETO01 - Exercicios sobre aula 2, com foco em um programa de conversão de temperatura usando String, double, int

---
+++ src/ExeeConversãoTemp.java
@@ -0,0 +1,17 @@
+public class ExeeConversãoTemp {
+    public static void main(String[] args) {
+        String programaTemperatura  = """
+                Neste programa iremos converter os valores de Celsius em Far;
+                esse programa é muito importante para os conhecimentos em
+                programação, feito por 
+                Pedro Henrique de Almeida Santos 
+                """;
+        System.out.println(programaTemperatura);
+        double  celsius = 21.7;
+        double conversao=   (celsius * 1.8 ) +32;
+        System.out.println(conversao);
+        int conversao2=   (int) (celsius * 1.8 ) +32;
+        System.out.println(conversao2);
+
+    }
+}
