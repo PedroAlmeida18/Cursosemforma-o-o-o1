a13445fd129beddbd3a94745b3dfd6514d94644f
PedroAlmeida18 <pedrohas35@gmail.com>
2023-06-04 17:08:36 -0300

    PROJETO01 - Aula 03, avaliação de filmes com laços de repetição

---
+++ src/Loop.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Loop {
+    public static void main(String[] args) {
+        Scanner leitura = new Scanner (System.in);
+        double Mediavaliaçao =0;
+        double nota=0 ;
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Qual a sua avaliação sobre o filme ?");
+            nota = leitura.nextDouble();
+            Mediavaliaçao += nota;
+
+        }System.out.println("Média das notas:"+ Mediavaliaçao/3.0);
+    }
+}
