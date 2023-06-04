646a1ce335fb41528d20d3865d52fad1d14d90fe
PedroAlmeida18 <pedrohas35@gmail.com>
2023-06-04 16:36:29 -0300

    PROJETO01 - Aula 03, criação de classes e incio sobre o input, com leitura e respostas data 04/06/2023

--- src/Leitura.java
+++ src/Leitura.java
@@ -1,9 +1,18 @@
 import java.util.Scanner;
-
-
 public class Leitura {
     public static void main(String[] args) {
         Scanner leitura = new Scanner (System.in);
+        System.out.println("Qual é o seu filme favorito ?");
+        String filme = leitura.nextLine();
+        System.out.println(filme);
+        System.out.println("Qual o ano de lançamento ?");
+        int anodeLançamento = leitura.nextInt();
+        System.out.println("Qual a sua avaliação sobre o filme ?");
+        double va = leitura.nextDouble();
+        System.out.println(filme);
+        System.out.println(anodeLançamento);
+        System.out.println(va);
+        leitura.close();
     }
 }
 
