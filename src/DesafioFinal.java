a9de1001ed3e6734829a4d5f0b730b5ae0b12d52
PedroAlmeida18 <pedrohas35@gmail.com>
2023-06-06 17:32:10 -0300

    PROJETO01 - Conclusão do curso : Java - criando sua primeira aplicação. Desafio final

--- src/DesafioFinal.java
+++ src/DesafioFinal.java
@@ -2,52 +2,55 @@ import java.util.Scanner;
 
 public class DesafioFinal {
     public static void main(String[] args) {
-        System.out.println("Bem vindo ao Banco Pe");
-        Scanner leitor = new Scanner(System.in);
-        int conta = 2500;
-        System.out.println("Digite seu nome abaixo :");
-        String nomeUser = leitor.nextLine();
-        System.out.println("Menu da conta de " + nomeUser);
-        if(nomeUser.equalsIgnoreCase("Pedro")){
-            System.out.println(  "User : " + nomeUser+ "\n" + "SALDO :" + conta + "\n"
-                     + "TIPO DE CONTA : CORRENTE "
-            );
-            System.out.println(" OPÇÕES DA CONTA, digite um número para atividade desejada : " +  "\n" + "1 - Consulta saldo " + "\n" + "2 - Deposito"
-            + "\n" + "3- Realizar transfêrencia "+"\n"+"4 - Encerrar atividade ");
-            int açao = leitor.nextInt();
-            if ( açao == 1){
-                System.out.println(" O SALDO DA CONTA É :"+ conta);
+        String nome = " PEDRO ";
+        Scanner Leitor = new Scanner(System.in);
+        String conta = " CORRENTE ";
+        double saldo = 1000;
+        int opção= 0 ;
+        System.out.println("_____________________________________________");
+        System.out.println("\n Nome do cliente:" + nome );
+        System.out.println("TIPO CONTA :"+ conta);
+        System.out.println("Saldo atual da conta :"+ saldo);
+        System.out.println("\n_____________________________________________");
+        System.out.println("Bem vindo ao menu ");
+        String menu  = """ 
+        *** Bem vindo ao banco 2WP ***
+        1 - Consultar Saldo 
+        2 - Transferência 
+        3 - Deposito 
+        4 - Fechar o app do Banco 
+        Resposta abaixo :
+        _________________________________
+        """;
+        while (opção != 4){
+            System.out.println(menu);
+            opção = Leitor.nextInt();
+            if ( opção == 1){
+                System.out.println(" O SALDO DA CONTA É :"+ saldo);
             }
-            if( açao== 3){
-                int transf = leitor.nextInt();
-                if ( conta >= transf){
-                    conta -= transf;
-                    System.out.println("A TRANSAÇÃO FOI REALIZADA SEU SALDO É :"+ conta);
-                }
-                if ( conta < transf){
-                    System.out.println("A  transferencia não foi possivel ");
-                }
-            }
-            if ( açao == 2 ){
-                int deposito = leitor.nextInt();
-                conta += deposito;
-                System.out.println(" O seu novo saldo com esse deposito é :" + conta);
-
-            }
-            if (açao== 4){
-                System.out.println(" A atividade será incerrada, tem certeza : ");
-                String certeza = leitor.nextLine();
-                if (certeza.equalsIgnoreCase("sim")){
-                    System.out.println("Atividade encerrada ");
+            if ( opção == 2 ){
+                System.out.println("Digite o valor da transfêrencia ");
+                int trans = Leitor.nextInt();
+                if ( trans <= saldo ){
+                    System.out.println(" Transfência é possível ");
+                    System.out.println("O valor da Transferência foi :" + trans);
+                    saldo -= trans;
                 }
                 else {
-                    System.out.println("Atividade continua, digite seu nome : ");
-                    nomeUser = leitor.nextLine();
-
-
+                    System.out.println("A TRANSFERENCIA É IMPOSSIVEL, SALDO INVALIDO,APENAS  " + saldo);
                 }
             }
+            if (opção==3 ){
 
+                System.out.println(" DIGITE O VALOR DO DEPOSITO : ");
+                int deposito = Leitor.nextInt();
+                System.out.println(" O DEPOSITO FOI REALIZADO!!!");
+                saldo += deposito;
+            } else if (opção!= 4) {
+                System.out.println("OPÇÃO INVÁLIDA ");
+            }
         }
+
     }
-}
+    }
+
